75dcd2154705327c29198555dbf9ba370cf775d4
Arif Widi Nugroho <arif@sainsmograf.com>
2012-10-16 23:33:22 +0700

    more reliable download

--- manga.py
+++ manga.py
@@ -3,217 +3,229 @@ import json
 import os
 import urllib2
 import glob
+import time
 
 from bs4 import BeautifulSoup
 
 
 class Mangas(object):
-	def __init__(self, mangas):
-		self.mangas = mangas
+    def __init__(self, mangas):
+        self.mangas = mangas
 
-	def find_manga(self, name):
-		manga_list = []
-		for manga_name in self.mangas.keys():
-			if name.lower() in manga_name.lower():
-				manga_list.append(manga_name)
+    def find_manga(self, name):
+        manga_list = []
+        for manga_name in self.mangas.keys():
+            if name.lower() in manga_name.lower():
+                manga_list.append(manga_name)
 
-		return manga_list
+        return manga_list
 
-	def get_manga(self, name):
-		return Manga(name, self.mangas[name])
+    def get_manga(self, name):
+        return Manga(name, self.mangas[name])
 
-	def get_manga_b64(self, b64_name):
-		name = base64.urlsafe_b64decode(b64_name)
-		return Manga(name, self.mangas[name])
+    def get_manga_b64(self, b64_name):
+        name = base64.urlsafe_b64decode(b64_name)
+        return Manga(name, self.mangas[name])
 
 
 class Manga(object):
-	def __init__(self, name, url):
-		self.name = name
-		self.b64name = base64.urlsafe_b64encode(name)
-		self.url = url
-		self.manga_path = os.path.join('download', self.name)
-		self.chapters_path = os.path.join(self.manga_path, 'chapters.json')
+    def __init__(self, name, url):
+        self.name = name
+        self.b64name = base64.urlsafe_b64encode(name)
+        self.url = url
+        self.manga_path = os.path.join('download', self.name)
+        self.chapters_path = os.path.join(self.manga_path, 'chapters.json')
 
-	def update(self):
-		try:
-			os.stat(self.manga_path)
-		except OSError:
-			os.mkdir(self.manga_path)
+    def update(self):
+        try:
+            os.stat(self.manga_path)
+        except OSError:
+            os.mkdir(self.manga_path)
 
-		print 'retrieving chapter list...'
-		chapters = []
-		html = fetch_html(self.url)
-		soup = BeautifulSoup(html)
-		ul = soup.select('.detail_list > ul')
-		for link in ul[0].find_all('a'):
-			name = link.get_text().strip()
-			url = link.get('href')
-			chapters.append((name, url))
+        print 'retrieving chapter list...'
+        chapters = []
+        html = fetch_html(self.url)
+        soup = BeautifulSoup(html)
+        ul = soup.select('.detail_list > ul')
+        for link in ul[0].find_all('a'):
+            name = link.get_text().strip()
+            url = link.get('href')
+            chapters.append((name, url))
 
-		chapters.reverse()
+        chapters.reverse()
 
-		with open(self.chapters_path, 'w') as f:
-			json.dump(chapters, f)
+        with open(self.chapters_path, 'w') as f:
+            json.dump(chapters, f)
 
-		print 'there are %d chapter' % len(chapters)
+        print 'there are %d chapter' % len(chapters)
 
-	def total_chapters(self):
-		with open(self.chapters_path) as f:
-			chapters = json.load(f)
+    def total_chapters(self):
+        with open(self.chapters_path) as f:
+            chapters = json.load(f)
 
-		return len(chapters)
+        return len(chapters)
 
-	def total_pages(self, chapter):
-		chapter_path = os.path.join(self.manga_path, str(chapter))
-		file_list = []
-		for item in os.listdir(chapter_path):
-			if os.path.isfile(os.path.join(chapter_path, item)):
-				file_list.append(item)
-		return len(file_list)
+    def total_pages(self, chapter):
+        chapter_path = os.path.join(self.manga_path, str(chapter))
+        file_list = []
+        for item in os.listdir(chapter_path):
+            if os.path.isfile(os.path.join(chapter_path, item)):
+                file_list.append(item)
+        return len(file_list)
 
-	def get_pages(self, chapter):
-		chapter_path = os.path.join(self.manga_path, str(chapter))
-		file_list = []
-		for item in os.listdir(chapter_path):
-			if os.path.isfile(os.path.join(chapter_path, item)):
-				file_list.append('%s/%d/%s' % (self.name, chapter, item))
-		file_list.sort()
-		return file_list
+    def get_pages(self, chapter):
+        chapter_path = os.path.join(self.manga_path, str(chapter))
+        file_list = []
+        for item in os.listdir(chapter_path):
+            if os.path.isfile(os.path.join(chapter_path, item)):
+                file_list.append('%s/%d/%s' % (self.name, chapter, item))
+        file_list.sort()
+        return file_list
 
-	def download_chapter(self, chapter):
-		with open(self.chapters_path) as f:
-			chapters = json.load(f)
+    def download_chapter(self, chapter):
+        with open(self.chapters_path) as f:
+            chapters = json.load(f)
 
-		name = chapters[chapter][0]
-		url = chapters[chapter][1]
+        name = chapters[chapter][0]
+        url = chapters[chapter][1]
 
-		print 'retrieving page list for chaper %d ...' % chapter
-		page_list = []
-		html = fetch_html(url)
-		soup = BeautifulSoup(html)
-		sel = soup.select('.wid60')
-		options = sel[0].select('option')
-		for option in options:
-			page_list.append(option.get('value'))
+        print 'retrieving page list for chaper %d ...' % chapter
+        page_list = []
+        html = fetch_html(url)
+        soup = BeautifulSoup(html)
+        sel = soup.select('.wid60')
+        options = sel[0].select('option')
+        for option in options:
+            page_list.append(option.get('value'))
 
-		print 'downloading images'
-		chapter_path = os.path.join(self.manga_path, str(chapter))
-		try:
-			os.stat(chapter_path)
-		except OSError:
-			os.mkdir(chapter_path)
+        print 'downloading images'
+        chapter_path = os.path.join(self.manga_path, str(chapter))
+        try:
+            os.stat(chapter_path)
+        except OSError:
+            os.mkdir(chapter_path)
 
-		print 'pages:', len(page_list)
-		existing_pages = glob.glob(os.path.join(chapter_path, '*.*'))
-		last_page = len(existing_pages)
+        print 'pages:', len(page_list)
+        existing_pages = glob.glob(os.path.join(chapter_path, '*.*'))
+        last_page = len(existing_pages)
 
-		for i, page in enumerate(page_list):
-			if i < last_page:
-				print 'skipping page', i
-				continue
-			html = fetch_html(page)
-			soup = BeautifulSoup(html)
-			imgs = soup.select('#viewer img')
-			img_url = imgs[0].get('src')
-			filename = '%03d_%s' % (i, os.path.split(img_url)[1])
-			img_path = os.path.join(chapter_path, filename)
-			
-			print 'downloading', img_path
-			try:
-				data = fetch_html(img_url)
-				with open(img_path, 'wb') as f:
-					f.write(data)
-			except urllib2.HTTPError, e:
-				print 'download failed: %s' % e
-		print 'DONE!'
+        for i, page in enumerate(page_list):
+            if i < last_page:
+                print 'skipping page', i
+                continue
+            html = fetch_html(page)
+            soup = BeautifulSoup(html)
+            imgs = soup.select('#viewer img')
+            img_url = imgs[0].get('src')
+            filename = '%03d_%s' % (i, os.path.split(img_url)[1])
+            img_path = os.path.join(chapter_path, filename)
+            
+            print 'downloading', img_path
+            try:
+                data = fetch_html(img_url)
+                with open(img_path, 'wb') as f:
+                    f.write(data)
+            except urllib2.HTTPError, e:
+                print 'download failed: %s' % e
+        print 'DONE!'
 
 
 def fetch_html(url):
-	print 'fetching', url, '...'
-	f = urllib2.urlopen(url)
-	return f.read()
+    for i in range(12):
+        try:
+            print 'fetching', url, '...'
+            f = urllib2.urlopen(url)
+            return f.read()
+        except urllib2.URLError, e:
+            print 'fetch failed! retrying...'
+            time.sleep(1)
+    raise e
 
 
 def get_mangas(html):
-	soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html)
 
-	manga = {}
+    manga = {}
 
-	for link in soup.find_all('a'):
-		if 'manga_info' in link.get('class', []):
-			name = link.get_text().strip()
-			url = link.get('href')
-			manga[name] = url
+    for link in soup.find_all('a'):
+        if 'manga_info' in link.get('class', []):
+            name = link.get_text().strip()
+            url = link.get('href')
+            manga[name] = url
 
-	return manga
+    return manga
 
 
 def update_manga_list():
-	'''Update manga database'''
-	data = fetch_html('http://www.mangahere.com/mangalist/')
-	
-	print 'parsing data...'
-	mangas = get_mangas(data)
+    '''Update manga database'''
+    data = fetch_html('http://www.mangahere.com/mangalist/')
+    
+    print 'parsing data...'
+    mangas = get_mangas(data)
 
-	with open('download/manga-list.json', 'w') as f:
-		json.dump(mangas, f)
+    with open('download/manga-list.json', 'w') as f:
+        json.dump(mangas, f)
 
-	print 'DONE!'
+    print 'DONE!'
 
 
 def load_mangas():
-	'''Load manga list from database'''
-	with open('download/manga-list.json') as f:
-		data = json.load(f)
+    '''Load manga list from database'''
+    with open('download/manga-list.json') as f:
+        data = json.load(f)
 
-	return Mangas(data)
+    return Mangas(data)
 
 
 def load_soup(path):
-	with open(path) as f:
-		data = f.read()
+    with open(path) as f:
+        data = f.read()
 
-	return BeautifulSoup(data)
+    return BeautifulSoup(data)
 
 
 def download_chapter(name, chapter, db_update=False):
-	m = load_mangas()
-	tor = m.get_manga(name)
-	if db_update:
-		tor.update()
-	tor.download_chapter(chapter)
+    m = load_mangas()
+    tor = m.get_manga(name)
+    if db_update:
+        tor.update()
+    tor.download_chapter(chapter)
 
 
 def download(name, start=0, db_update=True):
-	m = load_mangas()
-	mg = m.get_manga(name)
-	if db_update:
-		mg.update()
+    m = load_mangas()
+    mg = m.get_manga(name)
+    if db_update:
+        mg.update()
 
-	for chapter in xrange(mg.total_chapters()):
-		if chapter < start:
-			print 'skipping chapter', chapter
-			continue
-		mg.download_chapter(chapter)
+    for chapter in xrange(mg.total_chapters()):
+        if chapter < start:
+            print 'skipping chapter', chapter
+            continue
+        mg.download_chapter(chapter)
 
 def get_dowloaded_manga():
-	dir_list = []
-	for item in os.listdir('./download/'):
-		if os.path.isdir(os.path.join('./download', item)):
-			dir_list.append(item)
+    dir_list = []
+    for item in os.listdir('./download/'):
+        if os.path.isdir(os.path.join('./download', item)):
+            dir_list.append(item)
 
-	m = load_mangas()
-	manga_list = []
-	for manga_name in dir_list:
-		try:
-			mg = m.get_manga(manga_name)
-			manga_list.append(mg)
-		except KeyError:
-			pass
+    m = load_mangas()
+    manga_list = []
+    for manga_name in dir_list:
+        try:
+            mg = m.get_manga(manga_name)
+            manga_list.append(mg)
+        except KeyError:
+            pass
 
-	return manga_list
+    return manga_list
 
 def get_manga(name):
-	m = load_mangas()
-	return m.get_manga(name)
+    m = load_mangas()
+    return m.get_manga(name)
+
+def find_manga(name):
+    m = load_mangas()
+    return m.find_manga(name)
+    
--- webinterface.py
+++ webinterface.py
@@ -10,59 +10,59 @@ import manga
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-    	manga_list = manga.get_dowloaded_manga()
+        manga_list = manga.get_dowloaded_manga()
 
-    	def last_view_func(name):
-    		url = self.get_secure_cookie(name.replace(' ', '-'))
-    		if url is None:
-    			url = '/manga?name=%s&chapter=0&page=0' % name
-    		return url
+        def last_view_func(name):
+            url = self.get_secure_cookie(name.replace(' ', '-'))
+            if url is None:
+                url = '/manga?name=%s&chapter=0&page=0' % name
+            return url
 
         self.render('./templates/index.html', manga_list=manga_list, last_view=last_view_func)
 
 
 class MangaHandler(tornado.web.RequestHandler):
     def get(self):
-    	name = self.get_argument('name')
-    	chapter = int(self.get_argument('chapter', '0'))
-    	page = int(self.get_argument('page', '0'))
-    	m = manga.get_manga(name)
-    	pages = m.get_pages(chapter)
-    	total_pages = m.total_pages(chapter)
+        name = self.get_argument('name')
+        chapter = int(self.get_argument('chapter', '0'))
+        page = int(self.get_argument('page', '0'))
+        m = manga.get_manga(name)
+        pages = m.get_pages(chapter)
+        total_pages = m.total_pages(chapter)
 
-    	last_page_url = '/manga?name=%s&chapter=%d&page=%d' % (name, chapter, page)
-    	self.set_secure_cookie(name.replace(' ', '-'), last_page_url)
+        last_page_url = '/manga?name=%s&chapter=%d&page=%d' % (name, chapter, page)
+        self.set_secure_cookie(name.replace(' ', '-'), last_page_url)
 
-    	if (page + 1) >= total_pages:
-    		_chapter = chapter + 1
-    		_page = 0
-    	else:
-    		_chapter = chapter
-    		_page = page + 1
+        if (page + 1) >= total_pages:
+            _chapter = chapter + 1
+            _page = 0
+        else:
+            _chapter = chapter
+            _page = page + 1
 
-    	next_page_url = '?name=%s&chapter=%d&page=%d' % (name, _chapter, _page)
+        next_page_url = '?name=%s&chapter=%d&page=%d' % (name, _chapter, _page)
 
-    	if (page - 1) < 0:
-    		_chapter = chapter - 1
-    		if _chapter >= 0:
-    			_total_pages = m.total_pages(_chapter)
-    			_page = _total_pages - 1
-    		else:
-	    		_page = 0
-	    		_chapter = 0
-    	else:
-    		_chapter = chapter
-    		_page = page - 1
-    		
-    	prev_page_url = '?name=%s&chapter=%d&page=%d' % (name, _chapter, _page)
+        if (page - 1) < 0:
+            _chapter = chapter - 1
+            if _chapter >= 0:
+                _total_pages = m.total_pages(_chapter)
+                _page = _total_pages - 1
+            else:
+                _page = 0
+                _chapter = 0
+        else:
+            _chapter = chapter
+            _page = page - 1
+            
+        prev_page_url = '?name=%s&chapter=%d&page=%d' % (name, _chapter, _page)
 
         self.render('./templates/view.html',
-        			manga=m,
-        			chapter=chapter,
-        			page=page,
-        			page_image=self.static_url(pages[page]),
-        			next_page_url=next_page_url,
-        			prev_page_url=prev_page_url)
+                    manga=m,
+                    chapter=chapter,
+                    page=page,
+                    page_image=self.static_url(pages[page]),
+                    next_page_url=next_page_url,
+                    prev_page_url=prev_page_url)
 
 settings = {
     'cookie_secret': '61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=',
